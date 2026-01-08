2bb3c9887238deb8594bed65ee176052460dbac7
GuilhermeRDS <guilherme.rodriguesdasilva1010@gmail.com>
2026-01-08 15:44:34 -0300

    Criação da estrutura base do site.

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Home } from './Pages/Home'
+import { Login } from './Pages/Login'
+import { Register } from './Pages/Register'
+import { Lists } from './Pages/Lists'
+import { ListDetails } from './Pages/ListDetails'
+import { EditListItem } from './Pages/EditListItem'
+import { EditList } from './Pages/EditList'
+import ProtectedRoute from './Components/ProtectedRoute'
+import { Toaster } from 'react-hot-toast'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        
+        <Route path="/lists" element={
+          <ProtectedRoute><Lists /></ProtectedRoute>
+        } />
+        <Route path="/lists/:id" element={
+          <ProtectedRoute><ListDetails /></ProtectedRoute>
+        } />
+        <Route path="/lists/:id/edit" element={
+          <ProtectedRoute><EditList /></ProtectedRoute>
+        } />
+        <Route path="/lists/:id/create-item" element={
+          <ProtectedRoute><EditListItem /></ProtectedRoute>
+        } />
+      </Routes>
+      <Toaster />
+    </BrowserRouter>
   )
 }
 
