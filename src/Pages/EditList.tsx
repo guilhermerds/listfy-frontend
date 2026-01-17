a3a714bd937745a70b8ab0c67a420c503aaf433b
GuilhermeRDS <guilherme.rodriguesdasilva1010@gmail.com>
2026-01-17 14:58:21 -0300

    Removido páginas não utilizadas.

--- src/Pages/EditList.tsx
+++
@@ -1,8 +0,0 @@
-type Props = {
- 
-}
-export const EditList = ({}: Props) => {
-    return ( 
-        <h1>Edit List</h1>
-    );
-}
--- src/Pages/EditListItem.tsx
+++
@@ -1,9 +0,0 @@
-type Props = {
- 
-}
-
-export const EditListItem = ({}: Props) => {
-    return (  
-        <h1>Edit List Item</h1>
-    );
-}
